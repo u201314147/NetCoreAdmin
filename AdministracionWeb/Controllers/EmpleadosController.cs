682ad4f28f1b0d5ed4f8b12dd483e15cf79a1345
u201314147 <u201314147@upc.edu.pe>
2018-12-19 09:15:49 -0500

    Listado de empleados

---
+++ AdministracionWeb/Controllers/EmpleadosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdministracionWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdministracionWeb.Controllers
+{
+
+    [Route("Empleados")]
+    public class EmpleadosController : Controller
+    {
+        private DataContext db = new DataContext();
+
+       // [Route("")]
+       // [Route("~/")]
+
+        public IActionResult Empleados()
+        {
+            ViewBag.empleados = db.Empleados.ToList();
+            return View();
+        }
+    }
+}
---
+++ AdministracionWeb/Models/Empleados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdministracionWeb.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int id_empleado { get; set; }
+
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public int id_sexo { get; set; }
+        public int id_estadocivil { get; set; }
+        public int id_tiposangre { get; set; }
+        public DateTime fecha_nacimiento { get; set; }
+        public DateTime fecha_contrato { get; set; }
+        public string foto { get; set; }
+    }
+}
